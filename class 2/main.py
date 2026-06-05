97a71fcaa92734e92a02d8aedaf574dd2a85bdb4
KashanAdnan02 <fullstackdevkashan@gmail.com>
2026-06-05 17:13:20 +0500

    done

---
+++ class 2/main.py
@@ -0,0 +1,55 @@
+# x = 5 
+# name = 'Kashan' 
+# canDrive = False
+# canLearn = True
+
+# print(canDrive)
+# print(canLearn)
+# print(x)
+# print(name)
+# print(7)
+# print(10)
+
+# num = "90"
+# num2 = int(num)
+# print(num2)
+# print(int(num)) # "90"
+
+# print(type(num2))
+
+# int()
+# str()
+# bool()
+
+
+
+# num = "82"
+# num2 = int(num)
+
+# print(type(num))
+# print(type(num2))
+
+# class str
+
+
+
+# a = "Kashan"
+# a2 = bool(a)
+
+# print(type(a2))
+
+
+
+# a = 152
+# b = str(a)
+
+# print(type(b))
+
+
+# y = "Kashan" # str
+# a = True # bool
+# c = 1234567890 # int
+
+# print(type(a))
+
+
---
+++ class 3/main.py
@@ -0,0 +1,29 @@
+print("python classes")
+# ye hello world ka print he    
+#print("Hello World")
+
+# this is the code, we are learning python
+
+# as;lkdjf;alksjfdalsdf 
+
+
+# this is how you create variables
+# num = 183 + 90
+# num1 = 9
+# num2 = 8
+# print(19 / 2)
+# print(16 % 9)
+# print(18 // 2)
+
+# print(4 ** 4    )
+
+# Operators
+
+
+# +
+# -
+# *
+# /
+# **
+# //
+# %
