6ab76bba8a2549f97130789741c3384723d48828
S.Umar Farooq <eabumar54@gmail.com>
2026-06-06 18:22:31 +0500

    Add Python assignment tasks and print statements
    sorry pehle code attach kerna bhool gaya tha ab mene assignment code likh diya hai

---
+++ class 3/assignment.py
@@ -0,0 +1,50 @@
+print("Task No: 01")
+print("")
+print("My Name is Syed Umar Farooq")
+print("I am learning Python")
+print("Python is fun!")
+print("")
+print("Task No: 02")
+print("")
+name = "Syed Umar Farooq"
+age = 17
+city = "Karachi"
+
+print(name)
+print(age)
+print(city)
+print("")
+print("Task No: 03")
+print("")
+num1 = 15
+num2 = 10
+
+print(num1 + num2)
+print("")
+print("Task No: 04")
+print("")
+a = 8
+b = 5
+
+print(a * b)
+print("")
+print("Task No: 05")
+print("")
+print("")
+print(20 + 10)
+print(20 - 10)
+print(20 * 10)
+print(20 / 10)
+print(20 % 3)
+print(20 // 3)
+print(2 ** 4)
+print("")
+print("")
+print("Bonus Challenge")
+print("")
+print("")
+favorite_number = 5
+
+print(f"My favorite number is {favorite_number}")
+
+print("Task Fully Comleted")
